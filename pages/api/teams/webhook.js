6efb00acf9e01f3199485bfbd5680d5f7a6897b1
Rahul shinde <144519936+rahulsh19@users.noreply.github.com>
2025-04-28 11:18:19 +0530

    Update webhook.js

--- pages/api/teams/webhook.js
+++ pages/api/teams/webhook.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
       res.status(200).send(req.query.validationToken);
     } else {
       console.log('Notification received:', JSON.stringify(req.body));
-      res.status(202).send('Accepted'); // 202 is better for notification ack
+      res.status(200).send('OK',JSON.stringify(req.body)); // 202 is better for notification ack
     }
   } else {
     res.status(405).send('Method Not Allowed');
